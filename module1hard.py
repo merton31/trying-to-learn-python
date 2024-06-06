534bae214f64a8432c817ff94edf9dc53356dba5
merton31 <94224772+merton31@users.noreply.github.com>
2024-06-06 23:15:56 +0300

    module1hard.py

--- module1hard.py
+++ module1hard.py
@@ -1,6 +1,11 @@
-grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2],
+          [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-students_list = sorted(students)
-average_grades = [sum(grades)/len(grades) for grades in grades]
-final_list = dict(zip(students_list, average_grades))
-print(final_list)
+student_list = sorted(students)
+average = [(sum(grades[0]) / len(grades[0])),
+           (sum(grades[1]) / len(grades[1])),
+           (sum(grades[2]) / len(grades[2])),
+           (sum(grades[3]) / len(grades[3])),
+           (sum(grades[4]) / len(grades[4]))]
+average_student = dict (zip(student_list, average))
+print(average_student)
