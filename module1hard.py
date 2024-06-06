59eeb9db834a6eaf55c5a86086b209ce079f5b04
merton31 <94224772+merton31@users.noreply.github.com>
2024-06-06 23:12:53 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,6 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students_list = sorted(students)
+average_grades = [sum(grades)/len(grades) for grades in grades]
+final_list = dict(zip(students_list, average_grades))
+print(final_list)
