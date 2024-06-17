99915d9529c9f987f462c05dd735e61999d28e35
merton31 <94224772+merton31@users.noreply.github.com>
2024-06-17 12:15:59 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,17 @@
+def password(n):
+    pairs = []
+    used = set()
+    for i in range(1, 21):
+        for j in range(i + 1, 21):
+            if n % (i + j) == 0:
+                pairs.append(f'{i}{j}')
+                used.add(i)
+                used.add(j)
+    return ''.join(pairs)
+for n in range(3, 21):
+    result = password(n)
+    print(f'{n}: ', result)
+
+
+
+
